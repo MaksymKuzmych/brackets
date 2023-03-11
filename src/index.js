8d8cb040e98a7b8287bc1fcfa03a6bbd82576007
Maksym Kuzmych <kuzmichmaksym1997@gmail.com>
2023-03-11 13:50:33 +0200

    refactor: change let to const and get rid of logs

--- src/index.js
+++ src/index.js
@@ -1,40 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  let stack = []
-  let arr = str.split('')
-  let openBrackets = bracketsConfig.map((el) => el[0])
-  let closedBrackets = bracketsConfig.map((el) => el[1])
-  let brackets = {}
-  closedBrackets.forEach((el, idx) => (brackets[el] = openBrackets[idx]))
-  console.log(brackets)
+  const stack = [];
+  const arr = str.split('');
+  const openBrackets = bracketsConfig.map((el) => el[0]);
+  const closedBrackets = bracketsConfig.map((el) => el[1]);
+  const brackets = {};
+
+  closedBrackets.forEach((el, idx) => (brackets[el] = openBrackets[idx]));
+
   for (let i = 0; i < arr.length; i++) {
     if (openBrackets.includes(arr[i]) && closedBrackets.includes(arr[i])) {
       if (stack.length === 0) {
-        console.log(1)
-        stack.push(arr[i])
-        continue
+        stack.push(arr[i]);
+        continue;
       } else if (arr[i] !== stack[stack.length - 1] && closedBrackets.includes(stack[stack.length - 1])) {
-        console.log(2)
-        return false
+        return false;
       } else if (arr[i] === stack[stack.length - 1]) {
-        console.log(3)
-        stack.pop()
-        continue
+        stack.pop();
+        continue;
       }
     } else if (openBrackets.includes(arr[i])) {
-      console.log(4)
-      stack.push(arr[i])
-      continue
+      stack.push(arr[i]);
+      continue;
     } else if (closedBrackets.includes(arr[i])) {
-      console.log(5)
       if (brackets[arr[i]] !== stack[stack.length - 1]) {
-        console.log(6)
-        return false
+        return false;
       } else {
-        console.log(7)
-        stack.pop()
+        stack.pop();
       }
     }
   }
-  console.log('hey', stack)
-  return stack.length === 0 ? true : false
-}
+
+  console.log(stack);
+
+  return stack.length === 0;
+};
